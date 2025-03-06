9a3a5f8c5bbc7d3f4bdb5dfb4ace5f0c3582c6b1
Shreyas Kulkarni <shreyasnk532@gmail.com>
2025-03-06 15:28:08 -0800

    session storage for storing artsy token

--- assignment 3/routes/artsy/index.js
+++ assignment 3/routes/artsy/index.js
@@ -1,29 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const sessionMiddleware = require('../../middlewares/session-middleware');
 require('dotenv').config();
 
 BASE_URL = process.env.BASE_URL;
 CLIENT_ID = process.env.CLIENT_ID;
 CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-var token = undefined;
-
 async function getArtsyToken() {
     try {
         const response = await axios.post(BASE_URL + '/tokens/xapp_token?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET);
-        token = response.data.token;
-        console.log(token);
+        return response.data.token
     } catch (error) {
         console.error(error);
     }
 }
 
+// middleware to check if token is present
+router.use(sessionMiddleware);
+router.use(async (req, res, next) => {
+    if (req.session.token === undefined) {
+        console.log("Token not present, generating token");
+        req.session.token = await getArtsyToken();
+        next();
+    } else {
+        next();
+    }
+});
+
 // generate token
 router.get('/', async (req, res) => {
     try {
-        await getArtsyToken();
-        res.json({ "message": "Token fetched successfully" });
+        req.session.token = await getArtsyToken();
+        res.json({ "message": "Token generated" });
     } catch (error) {
         console.error(error);
     }
@@ -32,8 +42,8 @@ router.get('/', async (req, res) => {
 // search for artists
 router.get('/search_artist/:name', async (req, res) => {
     try {
-        if (token === undefined) {
-            await getArtsyToken();
+        if (req.session.token === undefined) {
+            res.json({ "message": "Please generate a token first" });
         }
         const name = req.params.name;
         if (name === undefined) {
@@ -41,7 +51,7 @@ router.get('/search_artist/:name', async (req, res) => {
         }
         const response = await axios.get(BASE_URL + '/search?q=' + name + '&size=10&type=artist', {
             headers: {
-                'X-Xapp-Token': token
+                'X-Xapp-Token': req.session.token
             }
         });
         artists = response.data['_embedded']['results']
@@ -54,8 +64,8 @@ router.get('/search_artist/:name', async (req, res) => {
 // get artist by id
 router.get('/get_artist/:id', async (req, res) => {
     try {
-        if (token === undefined) {
-            await getArtsyToken();
+        if (req.session.token === undefined) {
+            res.json({ "message": "Please generate a token first" });
         }
         const id = req.params.id;
         if (id === undefined) {
@@ -63,12 +73,12 @@ router.get('/get_artist/:id', async (req, res) => {
         }
         const response = await axios.get(BASE_URL + '/artists/' + id, {
             headers: {
-                'X-Xapp-Token': token
+                'X-Xapp-Token': req.session.token
             }
         });
         artist = response.data;
         res.json(artist);
-    }  catch (error) {
+    } catch (error) {
         console.error(error);
     }
 });
@@ -76,8 +86,8 @@ router.get('/get_artist/:id', async (req, res) => {
 // get artworks for artist
 router.get('/get_artist_artworks/:id', async (req, res) => {
     try {
-        if (token === undefined) {
-            await getArtsyToken();
+        if (req.session.token === undefined) {
+            res.json({ "message": "Please generate a token first" });
         }
         const id = req.params.id;
         if (id === undefined) {
@@ -85,7 +95,7 @@ router.get('/get_artist_artworks/:id', async (req, res) => {
         }
         const response = await axios.get(BASE_URL + '/artworks?artist_id=' + id + '&size=10', {
             headers: {
-                'X-Xapp-Token': token
+                'X-Xapp-Token': req.session.token
             }
         });
         artist = response.data['_embedded']['artworks'];
@@ -98,8 +108,8 @@ router.get('/get_artist_artworks/:id', async (req, res) => {
 // get genes for artist
 router.get('/get_artist_genes/:id', async (req, res) => {
     try {
-        if (token === undefined) {
-            await getArtsyToken();
+        if (req.session.token === undefined) {
+            res.send({ "message": "Please generate a token first" });
         }
         const id = req.params.id;
         if (id === undefined) {
@@ -107,7 +117,7 @@ router.get('/get_artist_genes/:id', async (req, res) => {
         }
         const response = await axios.get(BASE_URL + '/genes?artist_id=' + id, {
             headers: {
-                'X-Xapp-Token': token
+                'X-Xapp-Token': req.session.token
             }
         });
         artist = response.data['_embedded']['genes'];
