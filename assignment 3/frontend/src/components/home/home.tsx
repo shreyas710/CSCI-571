04d54ed777d78f22cbeb237e8b0a1045e4f1a25f
Shreyas Kulkarni <shreyasnk532@gmail.com>
2025-03-07 21:26:48 -0800

    home and navbar

--- assignment 3/frontend/src/components/home/home.tsx
+++ assignment 3/frontend/src/components/home/home.tsx
@@ -1,7 +1,24 @@
+import { Button, Container, Form } from "react-bootstrap";
+import { useState } from "react";
 export default function Home() {
+  const [search, setSearch] = useState<string>('');
+
     return (
-      <>
-        <h1>Home</h1>
-      </>
+      <Container>
+        <Form>
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            required
+            name='artist'
+            type='text'
+            id='search-bar'
+            className='search-bar w-75 mt-5'
+            placeholder='Please enter an artist name.'
+          />
+          <Button disabled={search == '' ? true : false} className="border-1">Search</Button>
+          <Button disabled={search == '' ? true : false} variant="secondary" onClick={() => setSearch('')}>Clear</Button>
+        </Form>
+      </Container>
     );
 }
