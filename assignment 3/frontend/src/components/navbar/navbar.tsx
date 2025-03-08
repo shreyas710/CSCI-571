41386f7d824f5f5ef7e229f20f95015f25b697a4
Shreyas Kulkarni <shreyasnk532@gmail.com>
2025-03-07 20:56:42 -0800

    basic navbar

--- assignment 3/frontend/src/components/navbar/navbar.tsx
+++ assignment 3/frontend/src/components/navbar/navbar.tsx
@@ -1,34 +1,29 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-    return (
-      <Navbar expand='lg' className='bg-body-tertiary'>
-        <Container>
-          <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='me-auto'>
-              <Nav.Link href='#home'>Home</Nav.Link>
-              <Nav.Link href='#link'>Link</Nav.Link>
-              <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-                <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-                <NavDropdown.Item href='#action/3.2'>
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href='#action/3.3'>
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href='#action/3.4'>
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
+  return (
+    <Navbar expand='lg' className='bg-body-tertiary'>
+      <Container fluid>
+        <Navbar.Brand href='/'>Artist Search</Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbarScroll' />
+        <Navbar.Collapse id='navbarScroll'>
+          <Nav className='me-auto my-2 my-lg-0' navbarScroll></Nav>
+          <Nav navbarScroll>
+            <Nav.Link as={Link} to='/'>
+              Search
+            </Nav.Link>
+            <Nav.Link as={Link} to='/login'>
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to='/register'>
+              Register
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
