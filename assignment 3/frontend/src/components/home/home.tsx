908bbc42cccb16e5337297354fc72dff31c793d7
Shreyas Kulkarni <shreyasnk532@gmail.com>
2025-03-07 23:28:43 -0800

    home page

--- assignment 3/frontend/src/components/home/home.tsx
+++ assignment 3/frontend/src/components/home/home.tsx
@@ -1,24 +1,43 @@
 import { Button, Container, Form } from "react-bootstrap";
 import { useState } from "react";
-export default function Home() {
-  const [search, setSearch] = useState<string>('');
 
-    return (
-      <Container>
-        <Form>
-          <input
+export default function Home() {
+  const [search, setSearch] = useState<string>("");
+
+  async function handleSubmit(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    console.log(search);
+  }
+
+  return (
+    <Container className='text-center'>
+      <Form>
+        <Form.Group
+          className='mb-3 mt-5 w-75 d-inline-flex'
+          controlId='exampleForm.ControlInput1'>
+          <Form.Control
+            type='text'
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            required
-            name='artist'
-            type='text'
-            id='search-bar'
-            className='search-bar w-75 mt-5'
             placeholder='Please enter an artist name.'
           />
-          <Button disabled={search == '' ? true : false} className="border-1">Search</Button>
-          <Button disabled={search == '' ? true : false} variant="secondary" onClick={() => setSearch('')}>Clear</Button>
-        </Form>
-      </Container>
-    );
+          <Button
+            variant='primary'
+            disabled={search == "" ? true : false}
+            type='submit'
+            style={{ borderRadius: 0 }}
+            onClick={(e) => handleSubmit(e)}>
+            Submit
+          </Button>
+          <Button
+            variant='secondary'
+            disabled={search == "" ? true : false}
+            onClick={() => setSearch("")}
+            style={{ borderRadius: "0 5px 5px 0" }}>
+            Clear
+          </Button>
+        </Form.Group>
+      </Form>
+    </Container>
+  );
 }
--- assignment 3/frontend/src/components/navbar/navbar.tsx
+++ assignment 3/frontend/src/components/navbar/navbar.tsx
@@ -11,7 +11,9 @@ export default function NavBar() {
   return (
     <Navbar expand='lg' className='bg-body-tertiary'>
       <Container fluid>
-        <Navbar.Brand href='/' className="ms-3 fs-4">Artist Search</Navbar.Brand>
+        <Navbar.Brand href='/' className='ms-3 fs-4'>
+          Artist Search
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls='navbarScroll' />
         <Navbar.Collapse id='navbarScroll'>
           <Nav className='me-auto my-2 my-lg-0' navbarScroll></Nav>
