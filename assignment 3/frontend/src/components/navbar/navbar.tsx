66c1e3455c2b6fa35b6dece8e07e18cd7a010d0b
Shreyas Kulkarni <shreyasnk532@gmail.com>
2025-03-07 21:18:13 -0800

    navbar

--- assignment 3/frontend/src/components/navbar/navbar.tsx
+++ assignment 3/frontend/src/components/navbar/navbar.tsx
@@ -2,8 +2,12 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 export default function NavBar() {
+  const [selectedTab, setSelectedTab] = useState("search");
+
   return (
     <Navbar expand='lg' className='bg-body-tertiary'>
       <Container fluid>
@@ -12,15 +16,45 @@ export default function NavBar() {
         <Navbar.Collapse id='navbarScroll'>
           <Nav className='me-auto my-2 my-lg-0' navbarScroll></Nav>
           <Nav navbarScroll>
-            <Nav.Link as={Link} to='/'>
-              Search
-            </Nav.Link>
-            <Nav.Link as={Link} to='/login'>
-              Login
-            </Nav.Link>
-            <Nav.Link as={Link} to='/register'>
-              Register
-            </Nav.Link>
+            <div className='me-4'>
+              {selectedTab == "search" ? (
+                <Button>Search</Button>
+              ) : (
+                <Nav.Link
+                  as={Link}
+                  to='/'
+                  className='fw-medium'
+                  onClick={() => setSelectedTab("search")}>
+                  Search
+                </Nav.Link>
+              )}
+            </div>
+            <div className='me-4'>
+              {selectedTab == "login" ? (
+                <Button>Log in</Button>
+              ) : (
+                <Nav.Link
+                  as={Link}
+                  to='/login'
+                  className='fw-medium'
+                  onClick={() => setSelectedTab("login")}>
+                  Log in
+                </Nav.Link>
+              )}
+            </div>
+            <div className='me-5'>
+              {selectedTab == "register" ? (
+                <Button>Register</Button>
+              ) : (
+                <Nav.Link
+                  as={Link}
+                  to='/register'
+                  className='fw-medium'
+                  onClick={() => setSelectedTab("register")}>
+                  Register
+                </Nav.Link>
+              )}
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
